0a832bb8de9562d683475865449806cac24d1ce3
Muhammad Nur Falah <nurfal321ff@gmail.com>
2026-07-12 18:07:44 +0700

    Refactor guess number game, improve feedback, and fix bug.
    Refactored guess number game to use a separate functions module for loading effects, improved user feedback messages and fixing hidden bug at line "97".

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
-import time
+import guess_game_functions as gg_function
 
 print("Let's play guess number!")
 
 playing: bool = True
+decorative_text: str = "-" * 100 
 
 while playing:    
     while True:
@@ -14,44 +15,42 @@ while playing:
             
             if max_number < 0:
                 print("Uppss, it seems you have entered negative value for the maximum number\nTry again!")
-                print("----------------------------------------------------------------------------------------------")
+                print(decorative_text)
 
             elif max_number == 0:
                 print("Sorry, the maximum number cannot be 0\nTry again!")
-                print("----------------------------------------------------------------------------------------------")
+                print(decorative_text)
 
             elif min_number == max_number:
                 print("Sorry, it seems the minimum number has the same value as maximum number\nTry again!")
-                print("----------------------------------------------------------------------------------------------")
+                print(decorative_text)
 
             elif min_number < 0:
                 print("Uppss, it seems you have entered negative value for the minimum number\nTry again!")
-                print("----------------------------------------------------------------------------------------------")
+                print(decorative_text)
 
             elif min_number == 0:
                 print("Sorry, the minimum number cannot be 0\nTry again!")
-                print("----------------------------------------------------------------------------------------------")
+                print(decorative_text)
 
             elif min_number > max_number:
-                print("Sorry, the minimum number cannot bigger than the maximum number!\nTry again!")
-                print("----------------------------------------------------------------------------------------------")
+                print("Sorry, the minimum number cannot be greater than the maximum number!\nTry again!")
+                print(decorative_text)
 
             elif max_number - min_number < 10:
                 print("Sorry, the difference between minimum number and maximum number must at least 10\nTry again!")
-                print("----------------------------------------------------------------------------------------------")
+                print(decorative_text)
 
             else:
-                print("----------------------------------------------------------------------------------------------")
+                print(decorative_text)
                 break
             
             
         except ValueError:
             print("It seems you entered wrong value. Please make sure you've entered number only and it's a whole number, please try again!")
-            print("----------------------------------------------------------------------------------------------")
+            print(decorative_text)
 
     random_number: int = random.randint(min_number, max_number)
-
-    wrong_guess: int = 0
 
     number_status: str = "Even" if random_number % 2 == 0 else "Odd"
 
@@ -61,18 +60,10 @@ while playing:
 
     point: int = 0
 
-    for i in range(6):
+    gg_function.loading_efect()
 
-        if point < 3:
-            point += 1
-        
-        elif point == 3:
-            point = 1
+    print(decorative_text)
 
-        time.sleep(0.5)
-        print(f"Deciding the number{"." * point}")
-
-    print("----------------------------------------------------------------------------------------------")
 
     while True:
         print(f"Guess the number between {min_number} and {max_number}")
@@ -82,63 +73,60 @@ while playing:
         
         except ValueError:
             print("Make sure to enter number only and it's a whole number!")
-            print("----------------------------------------------------------------------------------------------")
+            print(decorative_text)
             continue
 
         if guess < min_number or guess > max_number:
             print("Upps...Your guess is out of bound!.")
-            print("----------------------------------------------------------------------------------------------")
+            print(decorative_text)
             continue
 
-        elif guess > random_number:
+        if guess > random_number:
             print("Wrong guess, try again!")
-            print(f"you guessed \"{guess}\", it's to high!")
-            print("----------------------------------------------------------------------------------------------")
+            print(f"you guessed \"{guess}\", it's too high!")
+            print(decorative_text)
             guess_attempt += 1
-            wrong_guess += 1
 
         elif guess < random_number:
             print("Wrong guess, try again!")
-            print(f"you guessed \"{guess}\", it's to low!")
-            print("----------------------------------------------------------------------------------------------")
+            print(f"you guessed \"{guess}\", it's too low!")
+            print(decorative_text)
             guess_attempt += 1
-            wrong_guess += 1
         
         elif guess == random_number:
+            guess_attempt += 1
             break
 
-        if wrong_guess >= 3 and wrong_guess < 5:
+        if guess_attempt >= 3 and guess_attempt < 5:
             print(f"Here's a clue.\nThe number is {number_status}")
-            print("----------------------------------------------------------------------------------------------")
-            print()
+            print(decorative_text)
 
-        elif wrong_guess >= 5:
+        elif guess_attempt >= 5:
             print(f"Here's the clue.\nThe number is {number_status} and The number has \"{random_num_has}\" in it.")
-            print("----------------------------------------------------------------------------------------------")
-            print()
+            print(decorative_text)
 
     if guess_attempt == 1:
         print("Incredible!!! \nIt only took you 1 time to guess it")
 
     else:
-        print(f"You guessed it {guess_attempt} times")
+        print(f"You guessed it in {guess_attempt} attempts.")
         print(f"Well done, \"{random_number}\" is the correct number")
 
-    print("----------------------------------------------------------------------------------------------")
+    print(decorative_text)
 
     while True:
         play_again: str = input("Do you want to play again? (yes/no)\n>")
 
         if play_again.lower() == "yes" or play_again.lower() == "y":
-            print("----------------------------------------------------------------------------------------------")
+            print(decorative_text)
             break
 
         elif play_again.lower() == "no" or play_again.lower() == "n":
             print("aight then...have a nice day!")
-            print("----------------------------------------------------------------------------------------------")
+            print(decorative_text)
             playing = False
             break
 
         else:
             print("Make sure to input the right choice! \"yes\" or \"no\"")
-            print("----------------------------------------------------------------------------------------------")
+            print(decorative_text)
