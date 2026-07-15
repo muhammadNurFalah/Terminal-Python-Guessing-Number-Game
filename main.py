920203b5e2f7879fc22d81af8353e1ccad489ffb
Muhammad Nur Falah <nurfal321ff@gmail.com>
2026-07-15 11:56:30 +0700

    Refactor guessing game logic and input handling. Make it more readability.
    I changed the while loops into it's own function, make it more readability and shorter. It's easy to debugging

--- main.py
+++ main.py
@@ -1,119 +1,26 @@
 import random
+from guess_game_functions import decorative_text
 import guess_game_functions as gg_function
 
 print("Let's play guess number!")
 
 playing: bool = True
-decorative_text: str = "-" * 100 
+number_range: tuple[int,int] = gg_function.generate_min_and_max_num()
+min_number: int = number_range[0]
+max_number: int = number_range[1]
 
 while playing:    
-    while True:
-        try:
-            min_number: int = int(input("Select the number you want to begin with\n>"))
-
-            max_number: int = int(input("Select the number that will end with\n>"))
-            
-            if max_number < 0:
-                print("Uppss, it seems you have entered negative value for the maximum number\nTry again!")
-                print(decorative_text)
-
-            elif max_number == 0:
-                print("Sorry, the maximum number cannot be 0\nTry again!")
-                print(decorative_text)
-
-            elif min_number == max_number:
-                print("Sorry, it seems the minimum number has the same value as maximum number\nTry again!")
-                print(decorative_text)
-
-            elif min_number < 0:
-                print("Uppss, it seems you have entered negative value for the minimum number\nTry again!")
-                print(decorative_text)
-
-            elif min_number == 0:
-                print("Sorry, the minimum number cannot be 0\nTry again!")
-                print(decorative_text)
-
-            elif min_number > max_number:
-                print("Sorry, the minimum number cannot be greater than the maximum number!\nTry again!")
-                print(decorative_text)
-
-            elif max_number - min_number < 10:
-                print("Sorry, the difference between minimum number and maximum number must at least 10\nTry again!")
-                print(decorative_text)
-
-            else:
-                print(decorative_text)
-                break
-            
-            
-        except ValueError:
-            print("It seems you entered wrong value. Please make sure you've entered number only and it's a whole number, please try again!")
-            print(decorative_text)
-
-    random_number: int = random.randint(min_number, max_number)
-
-    number_status: str = "Even" if random_number % 2 == 0 else "Odd"
-
+    number_to_guess: int = random.randint(min_number, max_number)
+    random_num_has: str = random.choice(str(number_to_guess)) 
     guess_attempt: int = 0
-
-    random_num_has: str = random.choice(str(random_number)) 
-
-    point: int = 0
-
     gg_function.loading_efect()
-
-    print(decorative_text)
-
-
+    
     while True:
-        print(f"Guess the number between {min_number} and {max_number}")
-        
-        try:
-            guess: int = int(input(">"))
-        
-        except ValueError:
-            print("Make sure to enter number only and it's a whole number!")
-            print(decorative_text)
-            continue
+        guess_attempt = gg_function.generate_question(min_number= min_number, max_number= max_number, number_to_guess= number_to_guess)
+        break        
 
-        if guess < min_number or guess > max_number:
-            print("Upps...Your guess is out of bound!.")
-            print(decorative_text)
-            continue
-
-        if guess > random_number:
-            print("Wrong guess, try again!")
-            print(f"you guessed \"{guess}\", it's too high!")
-            print(decorative_text)
-            guess_attempt += 1
-
-        elif guess < random_number:
-            print("Wrong guess, try again!")
-            print(f"you guessed \"{guess}\", it's too low!")
-            print(decorative_text)
-            guess_attempt += 1
-        
-        elif guess == random_number:
-            guess_attempt += 1
-            break
-
-        if guess_attempt >= 3 and guess_attempt < 5:
-            print(f"Here's a clue.\nThe number is {number_status}")
-            print(decorative_text)
-
-        elif guess_attempt >= 5:
-            print(f"Here's the clue.\nThe number is {number_status} and The number has \"{random_num_has}\" in it.")
-            print(decorative_text)
-
-    if guess_attempt == 1:
-        print("Incredible!!! \nIt only took you 1 time to guess it")
-
-    else:
-        print(f"You guessed it in {guess_attempt} attempts.")
-        print(f"Well done, \"{random_number}\" is the correct number")
-
-    print(decorative_text)
-
+    gg_function.display_score(guess_attempt= guess_attempt, number_to_guess= number_to_guess)
+    
     while True:
         play_again: str = input("Do you want to play again? (yes/no)\n>")
 
