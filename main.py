c49cc030a3b067c1acbd50712b8c813ccee3c810
Muhammad Nur Falah <nurfal321ff@gmail.com>
2026-07-16 11:49:11 +0700

    Refactor main function for better structure. Adding a function for user to playing again
    Remove the while loops for asking the player to playing again and create a function for it. Also make the main code shorter

--- main.py
+++ main.py
@@ -1,39 +1,29 @@
+#File name: main.py
 import random
 from guess_game_functions import decorative_text
 import guess_game_functions as gg_function
 
-print("Let's play guess number!")
+def main() -> None:
+    print("Let's play guess number!")
 
-playing: bool = True
-number_range: tuple[int,int] = gg_function.generate_min_and_max_num()
-min_number: int = number_range[0]
-max_number: int = number_range[1]
+    playing: bool = True
 
-while playing:    
-    number_to_guess: int = random.randint(min_number, max_number)
-    random_num_has: str = random.choice(str(number_to_guess)) 
-    guess_attempt: int = 0
-    gg_function.loading_efect()
-    
-    while True:
+    while playing:    
+        number_range: tuple[int,int] = gg_function.generate_min_and_max_num()
+        min_number: int = number_range[0]
+        max_number: int = number_range[1]
+        number_to_guess: int = random.randint(min_number, max_number)
+        random_num_has: str = random.choice(str(number_to_guess)) 
+        guess_attempt: int = 0
+        
+        gg_function.loading_efect()
+        print(decorative_text)
+        
         guess_attempt = gg_function.generate_question(min_number= min_number, max_number= max_number, number_to_guess= number_to_guess)
-        break        
+        
+        gg_function.display_score(guess_attempt= guess_attempt, number_to_guess= number_to_guess)
+        
+        playing = gg_function.asking_for_playing_again()
 
-    gg_function.display_score(guess_attempt= guess_attempt, number_to_guess= number_to_guess)
-    
-    while True:
-        play_again: str = input("Do you want to play again? (yes/no)\n>")
-
-        if play_again.lower() == "yes" or play_again.lower() == "y":
-            print(decorative_text)
-            break
-
-        elif play_again.lower() == "no" or play_again.lower() == "n":
-            print("aight then...have a nice day!")
-            print(decorative_text)
-            playing = False
-            break
-
-        else:
-            print("Make sure to input the right choice! \"yes\" or \"no\"")
-            print(decorative_text)
+if __name__ == "__main__":
+    main()
